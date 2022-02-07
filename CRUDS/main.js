31e1cfce4d61d93b66c7dcb21d645427c47a1ff4
AhmedOmar100100 <ahmedomar234_sd@fcis.bsu.edu.eg>
2022-02-07 13:40:32 +0200

    final version

--- CRUDS/main.js
+++ CRUDS/main.js
@@ -1,15 +1,18 @@
 // variable setup
-title = document.getElementById("title");
-price = document.getElementById("price");
-taxes = document.getElementById("taxes");
-discount = document.getElementById("discount");
-total = document.getElementById("total");
-count = document.getElementById("count");
-category = document.getElementById("category");
-submit = document.getElementById("submit");
-thead = document.getElementById("thead");
-deleteAllSpace = document.getElementById("deleteAllSpace");
-
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let discount = document.getElementById("discount");
+let total = document.getElementById("total");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let submit = document.getElementById("submit");
+let thead = document.getElementById("thead");
+let deleteAllSpace = document.getElementById("deleteAllSpace");
+let mode = "create";
+let temp;
+let search = document.getElementById("search");
+let searchNotFound = document.getElementById("searchNotFound")
 
 // get total
 function getTotal() {
@@ -44,18 +47,39 @@ submit.onclick = function () {
     thead.style.visibility = "visible";
 
     product = {
-        title: title.value,
+        title: title.value.toLowerCase(),
         price: price.value,
         taxes: taxes.value,
         discount: discount.value,
         total: total.innerHTML,
         count: count.value,
-        category: category.value
+        category: category.value.toLowerCase()
     }
-    allProd.push(product);
-    saveToLocalStorage();
+    if (title.value != "" && category.value != "" && price.value != "" && count <= 100) {
+        if (mode == "create") {
+            if (product.count > 1) {
+                for (let i = 0; i < product.count; i++) {
+                    allProd.push(product);
+                }
+            } else {
+                allProd.push(product);
+            }
+        } else if (mode == "update") {
+            allProd[temp] = product;
+            mode = "create"
+            submit.innerHTML = "اضافة"
+            count.style.display = "block";
 
-    clearData();
+
+
+        }
+        saveToLocalStorage();
+        clearData();
+
+
+
+    }
+
     readData();
 
 
@@ -64,6 +88,7 @@ submit.onclick = function () {
 
 // clear inputs
 function clearData() {
+
     title.value = '';
     price.value = '';
     taxes.value = '';
@@ -71,6 +96,7 @@ function clearData() {
     total.innerHTML = '';
     category.value = '';
     count.value = '';
+    getTotal()
 }
 // read
 function readData() {
@@ -85,7 +111,7 @@ function readData() {
             <td>${allProd[index].taxes}</td>
             <td>${allProd[index].total}</td>
             <td>${allProd[index].category}</td>
-            <td><i class="far fa-edit" ></i></td>
+            <td><i class="far fa-edit" onclick="updateData(${index})" ></i></td>
             <td><i class="far fa-trash-alt" onclick="deleteData(${index})"></i></td>
         </tr>
         `;
@@ -94,7 +120,7 @@ function readData() {
     }
     tbody.innerHTML = table;
     if (allProd.length > 0) {
-        deleteAllSpace.innerHTML = '<button id="deleteAllBtn" onclick="deleteAll()">مسح الكل</button>';
+        deleteAllSpace.innerHTML = `<button id="deleteAllBtn" onclick="deleteAll()">مسح الكل (${allProd.length})</button>`;
     } else {
         deleteAllSpace.innerHTML = ""
     }
@@ -102,13 +128,11 @@ function readData() {
 }
 readData();
 
-// count
 // delete
 
 function clearThead() {
     if (allProd.length <= 0) {
         thead.style.visibility = "hidden"
-        console.log("hidden ")
 
     } else {
         thead.style.visibility = "visible"
@@ -136,5 +160,99 @@ function deleteData(i) {
 }
 clearThead()
 // update
+function updateData(i) {
+    temp = i;
+    mode = "update";
+    this.scroll({
+        top: 0,
+        behavior: "smooth"
+
+    })
+    title.value = allProd[i].title;
+    price.value = allProd[i].price;
+    taxes.value = allProd[i].taxes;
+    discount.value = allProd[i].discount;
+    total.innerHTML = allProd[i].total;
+    category.value = allProd[i].category;
+    count.style.display = "none"
+    if (price.value != '') {
+
+        total.style.backgroundColor = '#002400';
+    }
+    else {
+        total.innerHTML = '';
+        total.style.backgroundColor = '#F05454';
+    }
+    submit.innerHTML = "تحديث"
+
+}
 // search
+let searchMode = "title";
+function getSearchMode(id) {
+    search.focus()
+    if (id == 'searchByName') {
+        searchMode = "title";
+        search.placeholder = "بحث بالاسم"
+    } else {
+        searchMode = "category";
+        search.placeholder = "بحث بالتصنيف"
+
+    }
+    search.value = ""
+    console.log(searchMode)
+    readData()
+
+}
+
+function searchData(value) {
+    value = value.toLowerCase();
+    let table = "";
+    if (searchMode == "title") {
+        for (let i = 0; i < allProd.length; i++) {
+            if (allProd[i].title.includes(value)) {
+                table += `
+                    <tr>
+                        <td>${i + 1}</td>
+                        <td>${allProd[i].title}</td>
+                        <td>${allProd[i].price}</td>
+                        <td>${allProd[i].taxes}</td>
+                        <td>${allProd[i].total}</td>
+                        <td>${allProd[i].category}</td>
+                        <td><i class="far fa-edit" onclick="updateData(${i})" ></i></td>
+                        <td><i class="far fa-trash-alt" onclick="deleteData(${i})"></i></td>
+                    </tr>
+            `;
+                thead.style.visibility = "visible"
+                searchNotFound.style.display = "none"
+            } else {
+
+                thead.style.visibility = "hidden"
+                searchNotFound.style.display = "block"
+            }
+        }
+
+
+    } else {
+        for (let i = 0; i < allProd.length; i++) {
+            if (allProd[i].category.includes(value)) {
+                table += `
+                    <tr>
+                        <td>${i + 1}</td>
+                        <td>${allProd[i].title}</td>
+                        <td>${allProd[i].price}</td>
+                        <td>${allProd[i].taxes}</td>
+                        <td>${allProd[i].total}</td>
+                        <td>${allProd[i].category}</td>
+                        <td><i class="far fa-edit" onclick="updateData(${i})" ></i></td>
+                        <td><i class="far fa-trash-alt" onclick="deleteData(${i})"></i></td>
+                    </tr>
+            `;
+            }
+        }
+    }
+    tbody.innerHTML = table;
+
+
+}
 // clean
+
